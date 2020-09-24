17a0a6a295608d478814a8f34c7fa14e0f210497
Jozef Sovcik <jozef@sovcik.net>
2020-09-24 21:01:10 +0200

    date format function

--- src/_locales/date-fns.ts
+++ src/_locales/date-fns.ts
@@ -1,15 +1,15 @@
 import sk from 'date-fns/locale/sk';
 import en from 'date-fns/locale/en-US';
+import i18next from 'i18next';
+import dfns_format from 'date-fns/format';
 
 export namespace dateFns {
-    export function lang2Locale(lang?: string) {
-        switch (lang) {
-            case 'en':
-                return en;
-            case 'sk':
-                return sk;
-            default:
-                return en;
-        }
+    export const lang2Locale = {
+        en: en,
+        sk: sk,
+    };
+
+    export function format(datetime: Date | number, fmt: string) {
+        return dfns_format(datetime, fmt, { locale: lang2Locale[i18next.language] });
     }
 }
