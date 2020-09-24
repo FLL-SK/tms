01cfc2ef5f9182a4c269350c83bed6851571cb8a
Jozef Sovcik <jozef@sovcik.net>
2020-09-24 20:37:28 +0200

    removing app reducer

--- src/_actions/index.ts
+++ src/_actions/index.ts
@@ -1,6 +1,5 @@
 export * from './alert.actions';
 export * from './user.actions';
 export * from './rgScorer.actions';
-export * from './app.actions';
 export * from './event.actions';
 export * from './eventTeam.actions';
