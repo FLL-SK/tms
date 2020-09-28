ad2a26f590435172fe3eb5dc634c674185768cf2
Jozef Sovcik <jozef@sovcik.net>
2020-09-28 21:03:42 +0200

    button to card

--- src/_components/ProfileButton.tsx
+++ src/_components/ProfileButton.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export function ProfileButton({ profile }) {
     return (
-        <Button type="button" href={'/profile/' + profile._id} variant="outline-primary">
-            {profile.fullName}
-        </Button>
+        <Card style={{ width: '18rem', margin: '1rem' }}>
+            <Card.Body>
+                <Card.Title>
+                    <h5>{profile.fullName}</h5>
+                </Card.Title>
+                <Card.Link href="'/profile/' + profile._id">Profile</Card.Link>
+            </Card.Body>
+        </Card>
     );
 }
