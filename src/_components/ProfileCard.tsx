a16c075f7d38d6411046add6805a6016b34bec6e
Jozef Sovcik <jozef@sovcik.net>
2020-10-01 13:59:44 +0200

    renaming to ProfileCard

--- src/_components/ProfileCard.tsx
+++ src/_components/ProfileCard.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-export function ProfileButton({ profile }) {
+export function ProfileCard({ profile }) {
     return (
-        <Card style={{ width: '18rem', margin: '1rem' }}>
+        <Card style={{ width: '18rem', margin: '1rem', backgroundColor: 'lightcyan' }}>
             <Card.Body>
                 <Card.Title>
                     <h5>{profile.fullName}</h5>
                 </Card.Title>
-                <Card.Link href="'/profile/' + profile._id">Profile</Card.Link>
+                <Card.Link href={'/profile/' + profile._id}>Profile</Card.Link>
             </Card.Body>
         </Card>
     );
